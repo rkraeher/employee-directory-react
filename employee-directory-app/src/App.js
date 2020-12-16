89d45f1ddecc1a783151637977e5d9e6f3f5d813
Ru <rudikraeher@gmail.com>
2020-12-16 15:08:14 -0800

    footer

--- employee-directory-app/src/App.js
+++ employee-directory-app/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 // Import my pages
 // Import my components
 
@@ -10,6 +12,7 @@ function App() {
     <Router>
       <div>
         <Navbar />
+        <Footer />
       </div>
     </Router>
   );
