8fce0cc66180f7809dd164fa6f9d951e903a6ce7
Ru <rudikraeher@gmail.com>
2020-12-16 15:02:43 -0800

    setting up App.js and Navbar

---
+++ employee-directory-app/src/components/Navbar/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link className="navbar-brand" to="/">
+                Directory
+      </Link>
+            <div>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link
+                            to="/"
+                            className={
+                                window.location.pathname === "/" || window.location.pathname === "/general"
+                                    ? "nav-link active"
+                                    : "nav-link"
+                            }
+                        >
+                            General
+            </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                            to="/sort"
+                            className={window.location.pathname === "/sort" ? "nav-link active" : "nav-link"}
+                        >
+                            Sort
+            </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                            to="/filter"
+                            className={window.location.pathname === "/filter" ? "nav-link active" : "nav-link"}
+                        >
+                            Filter
+            </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
