6107021908fd1241cb7b11e83933d929e9f42872
Ru <rudikraeher@gmail.com>
2020-12-18 14:42:16 -0800

    built a working sort but it needs to be properly situated in the component architecture

--- src/pages/General.js
+++ src/pages/General.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Container from "../components/Container";
 import Row from "../components/Row";
-import Sort from "../components/Sort";
+//import Sort from "../components/Sort";
 import TableHeading from "../components/TableHeading";
 import API from "../utils/API";
 
 function General() {
     const [directory, setDirectory] = useState([]);
+    let criteria = true;
 
     useEffect(() => {
         API.fullDirectory()
@@ -16,6 +17,18 @@ function General() {
             .catch(err => console.log(err));
     }, []);
 
+    //This consoles correctly but having trouble avoiding an infinite loop when I try to render it
+    // Something to do with API call
+    // Perhaps this needs to be sent down as props and the manipulated so that I'm not re-rendering the page,
+    //because if I change the state I end up fetching again from the API.
+    // Send the directory down as props, sort it, send it back up and then the sorted
+    // array gets send down as props again to the next component.
+
+    function sorting() {
+        const sorted = [].concat(directory)
+            .sort((a, b) => a.last > b.last ? 1 : -1);
+        return setDirectory(sorted);
+    }
 
     return (
         <div>
@@ -24,12 +37,13 @@ function General() {
                 <table className="table table-dark m-3">
                     <TableHeading />
                     <tbody>
-                        {directory.map((employee, index) =>
-                            <Row
-                                directory={employee}
-                                index={index + 1}
-                            />
-                        )}
+                        {directory //instead of sorting from directory this will become the sorted array.
+                            .map((employee, index) =>
+                                <Row
+                                    directory={employee}
+                                    index={index + 1}
+                                />
+                            )}
                     </tbody>
                 </table>
             </Container>
